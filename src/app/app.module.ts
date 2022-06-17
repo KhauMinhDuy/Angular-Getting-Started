fbffe4064ed4bb0a7191a0dd29ed73f97f942300
khauminhduy <khauminhduy@gmail.com>
2022-06-17 13:01:36 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
+    WelcomeComponent,
     AppComponent,
     ProductListComponent,
     StarComponent,
     ConvertToSpacePipe,
+    ProductDetailComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent],
